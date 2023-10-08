de1da24cd0610c5424bcc2409f3d48faed8b86af
ishihara-fumiya <ishihara@corp-jjs.com>
2023-10-08 13:23:00 +0900

    メッセージボードを作成

---
+++ test.js
@@ -0,0 +1,11 @@
+// 配列を定義
+var arr = [1, 2, 3, 4, 5];
+
+// popメソッドを使用して、配列の最後の要素を取り出す
+var lastElement = arr.pop();
+
+// lastElementには取り出された最後の要素（5）が格納される
+console.log("Popped element:", lastElement); // 出力: Popped element: 5
+
+// arrは最後の要素が取り除かれた状態になる
+console.log("Updated array:", arr); // 出力: Updated array: [1, 2, 3, 4]
